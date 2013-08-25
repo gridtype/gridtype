932e95334c508bce5cce987587283590a18075da
Matthew White <mwhite7@lesley.edu>
2013-08-25 13:00:27 -0400

    changes to grunt.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,6 @@
 module.exports = function(grunt) {
     grunt.initConfig({
-<<<<<<< HEAD
-<<<<<<< HEAD
         pkg: grunt.file.readJSON('package.json'),		
-=======
-        pkg: grunt.file.readJSON('package.json'),
->>>>>>> 226e251d1842e0dff6de5f412ea94d41b36f5c6f
-=======
-        pkg: grunt.file.readJSON('package.json'),
->>>>>>> 226e251d1842e0dff6de5f412ea94d41b36f5c6f
         compass: {
             dist: {
                 options: {
@@ -47,24 +39,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint')
     grunt.loadNpmTasks('grunt-contrib-uglify')
     grunt.loadNpmTasks('grunt-contrib-compass')
-<<<<<<< HEAD
-<<<<<<< HEAD
-    grunt.loadNpmTasks('grunt-contrib-watch')
-    grunt.loadNpmTasks('grunt-contrib-copy')
-    grunt.loadNpmTasks('grunt-manifest-concat')
-
-=======
-=======
->>>>>>> 226e251d1842e0dff6de5f412ea94d41b36f5c6f
     grunt.loadNpmTasks('grunt-contrib-copy')
 	grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-manifest-concat')
-
-
-<<<<<<< HEAD
->>>>>>> 226e251d1842e0dff6de5f412ea94d41b36f5c6f
-=======
->>>>>>> 226e251d1842e0dff6de5f412ea94d41b36f5c6f
     grunt.registerTask('default', ['compass:dist', 'copy:dist', 'concatfest'])
     grunt.registerTask('release', ['compass:dist', 'copy:dist', 'concatfest'])
 
